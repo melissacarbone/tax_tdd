da8e535200c8298e8a447198126f6ac93599eb29
Melissa Carbone <mcarbone1@gmail.com>
2013-12-15 16:07:52 -0500

    fixed default values for missing data

--- employee.rb
+++ employee.rb
@@ -4,25 +4,25 @@ class Employee
   attr_reader :first_name, :last_name, :annual_income, :tax_paid, :tax_rate
 
   def initialize(first_name, last_name, annual_income, tax_paid, tax_rate)
-    if first_name == ''
+    if first_name == nil
       @first_name = '[First Name]'
     else
       @first_name = first_name
     end
 
-    if last_name == ''
+    if last_name == nil
       @last_name = '[Last Name]'
     else
       @last_name = last_name
     end
 
-    if annual_income == ''
+    if annual_income == nil
       @annual_income = 0
     else
       @annual_income = annual_income.to_i
     end
 
-    if tax_paid == ''
+    if tax_paid == nil
       @tax_paid = 0
     else
       @tax_paid = tax_paid.to_i
